635682379a7769fe93cc1d6b990b0fb3c943fed5
idosinchuk <igor.dosinchuk.ext@ing.com>
2020-02-24 20:06:30 +0100

    Add ResponseEntity to getAddress

--- src/main/java/com/idosinchuk/distributedtracing/address/controller/AddressController.java
+++ src/main/java/com/idosinchuk/distributedtracing/address/controller/AddressController.java
@@ -1,7 +1,9 @@
 package com.idosinchuk.distributedtracing.address.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,16 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class AddressController {
 
-    @Autowired
-    private AddressService addressService;
+	@Autowired
+	private AddressService addressService;
 
-    @GetMapping(value = "/addresses/{addressId}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Address getAddress(@PathVariable Integer addressId) {
+	@GetMapping(value = "/addresses/{addressId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Address> getAddress(@PathVariable Integer addressId) {
+		
 		log.info("Hello from Address Service with userId: {}", addressId);
-    	return addressService.getAddress(addressId);
-    }
+		Address response = addressService.getAddress(addressId);
+		
+		return new ResponseEntity<>(response, HttpStatus.OK);
+
+	}
 }
